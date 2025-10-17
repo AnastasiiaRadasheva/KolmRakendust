79f181f1fa2a0bd345a1b8ed5aa59f2e18c41842
opilane <opilane@HP-CZC2349HTC>
2025-10-17 12:24:38 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,79 @@
+namespace Kolm_rakendust
+{
+    public partial class Form1 : Form
+    {
+        TreeView tree;
+        Button btn;
+        Label lbl;
+        private pildiVaatamise pildiVaataja;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Height = 800;
+            this.Width = 1000;
+            this.Text = "Kolm rakendus ";
+            tree = new TreeView();
+            tree.Dock = DockStyle.Left;
+            tree.AfterSelect += Tree_AfterSelect;
+            TreeNode tn = new TreeNode("Elemendid");
+            tn.Nodes.Add(new TreeNode("Pildi vaatamise programm"));
+            tn.Nodes.Add(new TreeNode("Matemaatiline äraarvamismäng"));
+            tn.Nodes.Add(new TreeNode("Sarnaste piltide leidmise mäng"));
+            tn.Nodes.Add(new TreeNode("Välja"));
+
+
+            btn = new Button();
+            btn.Text = "Vajuta siia";
+            btn.Location = new Point(150, 30);
+            btn.Height = 30;
+            btn.Width = 100;
+            //pealkiri
+            lbl = new Label();
+            lbl.Text = "Elementide loomine c# abil";
+            lbl.Font = new Font("Arial", 24);
+            lbl.Size = new Size(400, 30);
+            lbl.Location = new Point(150, 0);
+            lbl.MouseHover += Lbl_MouseHover;
+            lbl.MouseLeave += Lbl_MouseLeave;
+            tree.Nodes.Add(tn);
+            this.Controls.Add(tree);
+            pildiVaataja = new pildiVaatamise(this);
+
+            pildiVaataja.Hide();
+        }
+
+
+        private void Tree_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Text == "Pildi vaatamise programm")
+            {
+                pildiVaataja.Show();
+
+            }
+            else if (e.Node.Text == "Matemaatiline äraarvamismäng")
+            {
+                this.Controls.Add(lbl);
+            }
+            else if (e.Node.Text == "Sarnaste piltide leidmise mäng")
+            {
+                this.Controls.Add(lbl);
+            }
+        }
+
+        private void Lbl_MouseLeave(object sender, EventArgs e)
+        {
+            lbl.BackColor = Color.Transparent;
+            Form1 Form = new Form1();
+            Form.Show();
+            this.Hide();
+
+
+        }
+
+        private void Lbl_MouseHover(object sender, EventArgs e)
+        {
+            lbl.BackColor = Color.FromArgb(200, 10, 20);
+        }
+
+    }
+}
---
+++ pildiVaatamise.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.Drawing;
+
+
+namespace Kolm_rakendust
+{
+    public class pildiVaatamise
+    {
+        private Button showButton;
+        private Button clearButton;
+        private Button backgroundButton;
+        private Button closeButton;
+        private Button filer;
+        private CheckBox stretchCheckBox;
+        private PictureBox pictureBox;
+        private OpenFileDialog openFileDialog;
+        private ColorDialog colorDialog;
+        private Control parentControl;
+
+        public pildiVaatamise(Control parent)
+        {
+            parentControl = parent;
+
+
+            openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.bmp;*.jpg;*.jpeg;*.png;*.gif|All files|*.*";
+
+            colorDialog = new ColorDialog();
+
+            showButton = new Button();
+            showButton.Text = "Vali pilt...";
+            showButton.Location = new Point(150, 10);
+            showButton.Size = new Size(100, 30);
+            showButton.Click += ShowButton_Click;
+
+            clearButton = new Button();
+            clearButton.Text = "Tühjenda pilt";
+            clearButton.Location = new Point(260, 10);
+            clearButton.Size = new Size(100, 30);
+            clearButton.Click += ClearButton_Click;
+
+
+            backgroundButton = new Button();
+            backgroundButton.Text = "Muuda taustavärvi";
+            backgroundButton.Location = new Point(370, 10);
+            backgroundButton.Size = new Size(120, 30);
+            backgroundButton.Click += BackgroundButton_Click;
+
+
+            closeButton = new Button();
+            closeButton.Text = "Sulge";
+            closeButton.Location = new Point(500, 10);
+            closeButton.Size = new Size(100, 30);
+            closeButton.Click += CloseButton_Click;
+
+
+            stretchCheckBox = new CheckBox();
+            stretchCheckBox.Text = "Rasta pilt";
+            stretchCheckBox.Location = new Point(610, 15);
+            stretchCheckBox.Size = new Size(100, 20);
+            stretchCheckBox.CheckedChanged += StretchCheckBox_CheckedChanged;
+
+
+            filer = new Button();
+            filer.Text = "Filtrid";
+            filer.Location = new Point(700, 10);
+            filer.Size = new Size(100, 30);
+            filer.Click += filer_Click;
+
+
+
+
+
+            pictureBox = new PictureBox();
+            pictureBox.Location = new Point(150, 50);
+            pictureBox.Size = new Size(560, 450);
+            pictureBox.BorderStyle = BorderStyle.Fixed3D;
+            pictureBox.SizeMode = PictureBoxSizeMode.Normal;
+
+
+            parentControl.Controls.Add(showButton);
+            parentControl.Controls.Add(clearButton);
+            parentControl.Controls.Add(backgroundButton);
+            parentControl.Controls.Add(closeButton);
+            parentControl.Controls.Add(stretchCheckBox);
+            parentControl.Controls.Add(pictureBox);
+        }
+        private void filer_Click(object sender, EventArgs e)
+        {
+            string[] Valikud =
+            {
+                "Tumedam.",
+                "Heledam!",
+                "Jätka, sa teed suurepärast tööd!",
+                "Kas tead? Juhuslikud sõnumid on lõbusad.",
+                "Siin on väike üllatus!"
+            };
+        }
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox.Load(openFileDialog.FileName);
+            }
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+
+            pictureBox.Image = null;
+        }
+
+        private void BackgroundButton_Click(object sender, EventArgs e)
+        {
+
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox.BackColor = colorDialog.Color;
+            }
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+
+            if (parentControl is Form form)
+            {
+                form.Close();
+            }
+        }
+
+        private void StretchCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (stretchCheckBox.Checked)
+                pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            else
+                pictureBox.SizeMode = PictureBoxSizeMode.Normal;
+        }
+
+        public void Show()
+        {
+            showButton.Visible = true;
+            clearButton.Visible = true;
+            backgroundButton.Visible = true;
+            closeButton.Visible = true;
+            stretchCheckBox.Visible = true;
+            pictureBox.Visible = true;
+        }
+
+        public void Hide()
+        {
+            showButton.Visible = false;
+            clearButton.Visible = false;
+            backgroundButton.Visible = false;
+            closeButton.Visible = false;
+            stretchCheckBox.Visible = false;
+            pictureBox.Visible = false;
+        }
+    }
+}
